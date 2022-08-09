27a3b9256bc7278d6c63706d38758f17d107870d
Rahul <aarjayjangir@gmail.com>
2022-08-09 18:42:22 +0530

    commit on 09 august

---
+++ README.md
@@ -0,0 +1,21 @@
+# FirstApp
+## 09 august 2022
+
+### practice
+install node js
+intsall npm
+install @angular/cli
+create ng new @my-first-app
+in @my-first-app [
+    create header component
+    create footer componet
+]
+code :-https://github.com/aarjaycreation/angular_learning.git
+
+### theory
+intruduction 
+
+
+
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './My-Component/header/header.component';
+import { FooterComponent } from './My-Component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
